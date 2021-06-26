d4e9256eb929b05c26989de1225fa9ead2903adc
jaankuu <jaankuusaru@gmail.com>
2021-06-26 17:24:35 +0200

    Created create-event page with a form, and a corresponding store action to send data to backend

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import events from "./events/reducer"
 import user from "./user/reducer"
+import eventDetails from "./eventDetails/reducer"
 
 export default combineReducers({
   events,
-  user
+  user,
+  eventDetails
 });
